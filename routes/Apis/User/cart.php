1377f4264f286e6d90d84abbeb64cf87c9b665a5
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-08-04 05:37:06 +0300

    up

--- routes/Apis/User/cart.php
+++ routes/Apis/User/cart.php
@@ -7,13 +7,17 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('web')
-    ->controller(CartController::class)
-    ->prefix('/user')
-    ->group(function () {
-        Route::get('/show/his/cart', 'show');
-        Route::post('/add/book', 'addBook');
-        Route::post('/update/book/quantity', 'updateBookQuantity');
-        Route::post('/remove/book', 'removeBook');
-        Route::post('/clear/cart', 'clearCart');
-    });
+Route::controller(CartController::class)
+->prefix('/user')
+->group(
+    function () {
+   Route::get('/show/his/cart', 'show');
+   Route::post('/add/book', 'addBook');
+   Route::post('/update/book/quantity/book', 'updateBookQuantity');
+    Route::post('/remove/book', 'removeBook');
+    Route::post('/clear/cart', 'clearCart');
+ 
+
+
+
+});
